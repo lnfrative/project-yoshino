b81e4113c82eb7dc73569004fce4363a0c1e9e63
Christopher Díaz <nealthmc@gmail.com>
2023-01-15 21:24:10 -0500

    create image controller

---
+++ src/main/java/controllers/CreateImageController.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class CreateImageController {
+    @FXML
+    private TextField descriptionField;
+    
+    @FXML
+    private TextField lugarField;
+    
+    @FXML
+    private TextField fechaField;
+    
+    @FXML
+    private TextField imageField;
+    
+    public void onCreate() {
+        
+    }
+    
+    public void onCancel() {
+        
+    }
+}
